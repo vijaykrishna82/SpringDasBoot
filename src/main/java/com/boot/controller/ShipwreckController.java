2dbd3b85e762b640c52c4d008550e0f4b6a71a92
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2016-05-17 08:01:11 -0400

    Integrate shipwreck repository

--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -3,40 +3,51 @@ package com.boot.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
 
 @RestController
 @RequestMapping("api/v1")
 public class ShipwreckController {
+	
+	@Autowired
+	private ShipwreckRepository ShipwreckRepository;
+	
 
 	@RequestMapping(value="shipwrecks", method=RequestMethod.GET)
 	public List<Shipwreck> list(){
-		return ShipwreckStub.list();
+		return ShipwreckRepository.findAll();
 	}
 	
 	@RequestMapping(value="shipwrecks/{id}", method=RequestMethod.GET)
 	public Shipwreck get(@PathVariable Long id){
-		return ShipwreckStub.get(id);
+		return ShipwreckRepository.findOne(id);
 	}
 	
 	@RequestMapping(value="shipwrecks", method=RequestMethod.POST)
 	public Shipwreck create(@RequestBody Shipwreck shipwreck){
-		return ShipwreckStub.create(shipwreck);
+		return ShipwreckRepository.saveAndFlush(shipwreck);
 	}
 	
 	@RequestMapping(value="shipwrecks/{id}", method=RequestMethod.PUT)
 	public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck){
-		return ShipwreckStub.update(id, shipwreck);
+		Shipwreck existingShipwreck = ShipwreckRepository.findOne(id);
+		BeanUtils.copyProperties(shipwreck, existingShipwreck);
+		return ShipwreckRepository.saveAndFlush(existingShipwreck);
 	}
 	
 	@RequestMapping(value="shipwrecks/{id}",method=RequestMethod.DELETE)
 	public Shipwreck delete(@PathVariable Long id)	{
-		return ShipwreckStub.delete(id);
+		Shipwreck existingShipwreck = ShipwreckRepository.findOne(id);
+		ShipwreckRepository.delete(existingShipwreck);
+		return existingShipwreck;
 	}
 	
 	
